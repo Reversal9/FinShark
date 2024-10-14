14a14634e18e6f3612ff109de2c49bfb9c04fa0f
jonne <jonnenc@gmail.com>
2024-10-13 23:53:43 -0400

    add login

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73faebd967358014e2230caf3e6391810a9b76d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d35fc5be869f0d493157c1130493338e9fd210b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
